5df3dbf5ce696d8bc20f0acc2f63d9f1bbe86c8c
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-08-04 02:49:04 +0700

    Add functor

---
+++ bonuses/functor.js
@@ -0,0 +1,15 @@
+const stringFunctor = (value, fn) => {
+  let chars = value.split("");
+  return chars
+    .map(char => {
+      return String.fromCharCode(fn(char.charCodeAt(0)));
+    })
+    .join("");
+};
+
+const plus1 = value => value + 1;
+const minus1 = value => value - 1;
+
+[3, 4].map(plus1); // returns [4,5]
+console.log(stringFunctor("ABC", plus1)); //  returns "BCD"
+console.log(stringFunctor("XYZ", minus1)); // returns "WXY"
