06098b7c29895dc4e8f412265ab6b6021b0c7d20
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-09-17 21:49:12 +0700

    Add closure cases

--- closure.js
+++ closure.js
@@ -1,5 +1,34 @@
-let name = "budi";
-const greet = () => console.log(`hello ${name}!`);
+// let name = "budi";
+// const greet = () => console.log(`hello ${name}!`);
 
-name = "suci";
-greet();
+// name = "suci";
+// greet();
+
+// const init = () => {
+//   return name => console.log(name);
+// };
+
+// let showName = init();
+
+// showName("Budi");
+
+// const greet = time => {
+//   return name => {
+//     console.log(`Hello, ${name}. Good ${time}`);
+//   };
+// };
+
+// let morning = greet("Morning");
+
+// morning("Matt");
+
+let add = (() => {
+  let counter = 0;
+  return () => {
+    return console.log(++counter);
+  };
+})();
+
+add();
+add();
+add();
